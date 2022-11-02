1d6af477697063407416b0eff5f3468db5b26bc4
Moshe Rubin <mosherubin137@gmail.com>
2022-11-02 10:56:11 -0500

    Add `path` function

--- IDrive-Sync/PowerShell/profile.ps1
+++ IDrive-Sync/PowerShell/profile.ps1
@@ -10,6 +10,9 @@ $env:PIP_NO_WARN_SCRIPT_LOCATION=$false  # gh: pypa/pip#6372
 # Aliases
 New-Alias e explorer.exe
 New-Alias g git
+function path{
+    $env:path -split ";"
+}
 
 # https://devblogs.microsoft.com/scripting/increase-powershell-command-history-to-the-max-almost/
 $MaximumHistoryCount=32767
